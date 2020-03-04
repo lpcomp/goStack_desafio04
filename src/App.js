86211703ae90dc2501405123d8d42a5b91db7ab0
Luiz Paulo Baçal de Vasconcelos <luizv993@gmail.com>
2020-03-04 18:58:36 -0300

    molde para o desafio 04

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./App.css";
+
+import profile from "./assets/avatar.jpg";
+
+function App() {
+  return <img width="20%" src={profile} />;
+}
+
+export default App;
